8fe6920a154df45ebb3f578637c9d2351d7dcd1e
Kris <xoldik12@gmail.com>
2026-09-21 11:56:06 +0300

    Add project files.

---
+++ lab1.1/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace lab1._2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int a = 5;
+            int b = 6;
+            int result = ConsoleApp1.Program.Example(a, b);
+            Assert.AreEqual(11, result);
+        }
+    }
+}
